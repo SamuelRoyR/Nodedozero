53bc20f68850d6e7e990b80c7c48180152c02eb5
SamuelRoyR <samuel100rr@gmail.com>
2025-09-04 10:26:44 -0300

    mudanças na host

--- database-postgres.js
+++ database-postgres.js
@@ -1,14 +1,14 @@
 import { randomUUID } from "node:crypto"
-import {sql} from './db.js'
+import {pool} from './db.js'
 
 export class DatabasePostgres {
    async list(search ){
     let videos
 
     if(search){
-        videos = await sql`SELECT * FROM videos WHERE title ILIKE ${'%' + search + '%'}`
+        videos = await pool.query(`SELECT * FROM videos WHERE title ILIKE $1`, ['%' + search + '%'])
     }else{
-        videos = await sql`SELECT * FROM videos`
+        videos = await pool.query(`SELECT * FROM videos`)
     }
     return videos
     }
@@ -18,15 +18,15 @@ export class DatabasePostgres {
 
       const {title, description, duration} = video
 
-      await sql`INSERT INTO videos (id, title, description, duration) VALUES (${videoId}, ${title}, ${description}, ${duration})`
+      await pool.query(`INSERT INTO videos (id, title, description, duration) VALUES ($1, $2, $3, $4)`, [videoId, title, description, duration])
     }
     
     async update(id, video){
         const {title, description, duration} = video
 
-        await sql`UPDATE videos SET title = ${title}, description = ${description}, duration = ${duration} WHERE id = ${id}`
+        await pool.query(`UPDATE videos SET title = $1, description = $2, duration = $3 WHERE id = $4`, [title, description, duration, id])
     }
     async delete(id){
-        await sql`DELETE FROM videos WHERE id = ${id}`
+        await pool.query(`DELETE FROM videos WHERE id = $1`, [id])
     }
 }
